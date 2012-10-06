e48bcd127f7e9b7528936164a96eb6a2f3655402
Ajay Singh <ajays@yahoo-inc.com>
2012-10-06 21:25:07 +0530

    Stripping the error

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,34 +6,28 @@ import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
 
-  private static Object CREATED = null;
-
   @BeforeClass
   protected void beforeClass() {
     log();
-    if (CREATED != null) {
-      throw new IllegalStateException("There exists an object.");
-    }
-    CREATED = new Object();
   }
 
   @AfterClass(alwaysRun = true)
   protected void afterClass() {
     log();
-    CREATED = null;
   }
 
   protected void log() {
     StackTraceElement element = new Throwable().getStackTrace()[1];
     String className = getCallerSimpleClassName(element);
-    System.out.printf("%12s(%s) - %s%n", className, getClass().getSimpleName(), element.getMethodName());
+    System.out.printf("%s %12s(%s) - %s%n", Thread.currentThread().getName(), className, getClass().getSimpleName(),
+        element.getMethodName());
   }
 
   protected void log(String parameter) {
     StackTraceElement element = new Throwable().getStackTrace()[1];
     String className = getCallerSimpleClassName(element);
-    System.out
-        .printf("%12s(%s) - %s - %s%n", className, getClass().getSimpleName(), element.getMethodName(), parameter);
+    System.out.printf("%s %12s(%s) - %s - %s%n", Thread.currentThread().getName(), className, getClass()
+        .getSimpleName(), element.getMethodName(), parameter);
   }
 
   private String getCallerSimpleClassName(StackTraceElement element) {
