5491c5c9b5a4627770dc9803fede682256e64cab
Ajay Singh <ajays@yahoo-inc.com>
2012-10-06 18:04:53 +0530

    meaningful name

--- src/test/java/tests/Actual1Test.java
+++ src/test/java/tests/Actual1Test.java
@@ -1,13 +1,40 @@
 package tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
 public class Actual1Test extends BaseTest {
+
+  private static final String PARAMETERS = "parameters";
+
+  @DataProvider(name = PARAMETERS)
+  public Object[][] parameters() {
+    return new Object[][]{
+        new Object[]{
+          "one"
+        }, new Object[]{
+          "two"
+        }, new Object[]{
+          "three"
+        }, new Object[]{
+          "four"
+        }
+    };
+  }
 
   @Test
   public void test() {
     System.out.printf("%12s - %s%n", Actual1Test.class.getSimpleName(), "test");
   }
 
+  @Test(dependsOnMethods = "test")
+  public void test3() {
+    System.out.printf("%12s - %s%n", Actual1Test.class.getSimpleName(), "test3");
+  }
+
+  @Test(dataProvider = PARAMETERS)
+  public void test4(String parameter) {
+    System.out.printf("%12s - %s - %s%n", Actual1Test.class.getSimpleName(), "test4", parameter);
+  }
 }
---
+++ src/test/java/tests/Actual2Test.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+
+public class Actual2Test extends BaseTest {
+
+  @Test
+  public void test() {
+    System.out.printf("%12s - %s%n", Actual2Test.class.getSimpleName(), "test");
+  }
+
+}
